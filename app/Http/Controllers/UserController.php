47599f2884384fbbf1273d16d73552e234e79392
abdur_rahim <rahimbd91@gmail.com>
2023-11-11 19:19:06 +0600

    My First laravel Project

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function sayHello()
+    {
+        return 'Hello, Dear Instructor! This is my Laravel Project. Thank You';
+    }
+}
